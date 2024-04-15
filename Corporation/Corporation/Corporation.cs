3cb07a00912c4c246663c56fc31687542cf9884f
Irene Smith <irenesmith13@gmail.com>
2024-04-14 20:55:00 -0400

    First commit

---
+++ Corporation/Corporation/Corporation.cs
@@ -0,0 +1,3 @@
+﻿/// Corporation - a game based on The Corporation in Tim Hartnell's book,
+/// Executive games for the IBM PC & XT. Translated from interpreted BASIC to C#
+/// 
---
+++ Corporation/Corporation/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporation
+{
+    public class Game
+    {
+        public Single Loan { get; set; }
+        public Single BankBalance { get; set; }
+        public Single Capital { get; set; }
+
+        private Random randNum = new Random();
+
+        public void NewGame()
+        {
+
+        }
+        public Game()
+        {
+            Loan = 1000;
+            BankBalance = 0;
+        }
+
+    }
+    
+}
